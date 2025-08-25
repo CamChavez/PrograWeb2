ecba06da8b83fb8f0a1386c0cf6072f61c9a9012
CamChavez <camilachavez320@aragon.unam.mx>
2025-08-24 21:54:59 -0600

    tarea 2 primeros console.log

---
+++ tarea2/primeros.js
@@ -0,0 +1,21 @@
+        console.log("Hola Mundo");
+        console.log(123456);
+        console.log(true);
+        var saludo = "Hola";
+        console.log(saludo);
+        console.log([1,2,3,4,5]);
+        console.log({nombre: "Juan", edad: 30, pais: "México"});
+        console.table({nombre: "Juan", edad: 30, pais: "México"});
+        console.error("Esto es un mensaje de error");
+        console.warn("Esto es una advertencia");
+        console.time("Tiempo de ejecución");
+        console.log(2+2)
+        console.log(3+89);
+        console.log(56*4);
+        console.log(45/5);
+        console.log(5-2);
+        console.log(5%2);
+        console.log(5**3);
+        function responder(pregunta, respuesta) {
+                console.log(pregunta + " → " + respuesta);
+        }
